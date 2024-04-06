9fee211c6a1b34788eb58c165c884964597aa35a
John Siddoway <john.pez.s@gmail.com>
2024-04-06 11:16:09 -0700

    Start building out the button examples.

--- src/components/panels/normal-panel.tsx
+++ src/components/panels/normal-panel.tsx
@@ -1,5 +1,12 @@
 import { PanelProps } from "./panel-props";
+import styles from "./normal-panel.module.scss";
 
 export function NormalPanel(props: PanelProps) {
-    return <div>Normal Example Goes Here</div>;
+    const buttons = props.themeColors.map((value, index) => {
+        return <button key={index} className={styles.button}>{value.backgroundColor}</button>;
+    })
+    return <div>
+        <h3 className={styles.heading}>Normal Buttons</h3>
+        {buttons}
+    </div>;
 }
